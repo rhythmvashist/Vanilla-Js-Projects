1e613a43aaffd52b534c6afbd8940e58715292c1
Rhythmvashist <vashistrhythm1@gmail.com>
2019-10-05 22:47:56 -0700

    adde some UI enhancement

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -1,7 +1,18 @@
 
-document.getElementById('loan-form').addEventListener('submit',calculateResult);
+//document.getElementById('loan-form').addEventListener('submit',calculateResult);
 
-function calculateResult(e){
+document.getElementById('loan-form').addEventListener('submit',function(e) {
+  //hide results
+  document.getElementById('result').style.display='none';
+
+  // show loader 
+  document.getElementById('loading').style.display='block';
+  setTimeout(calculateResult,2000);
+  e.preventDefault();
+
+})
+
+function calculateResult(){
   console.log('calculate called');
 
   const amt=document.getElementById('amount');
@@ -13,7 +24,7 @@ function calculateResult(e){
   const totalint=document.getElementById('total-interest');
 
   const principal=parseFloat(amt.value);
-  const calculatedint=parseFloat(interest.value).toFixed(2);
+  const calculatedint=parseFloat(interest.value).toFixed(2)/100/12;
   const calculatedPayment=parseFloat(years.value)*12;
 
   const x=Math.pow(1+calculatedint,calculatedPayment);
@@ -24,29 +35,34 @@ function calculateResult(e){
     monthlypayment.value=monthly.toFixed(2);
     totalpayment.value=(monthly*calculatedPayment).toFixed(2);
     totalint.value=((monthly*calculatedPayment)-principal).toFixed(2);
+
+    document.getElementById('result').style.display='block';
+
+    document.getElementById('loading').style.display='none';
   }
   else{
-
     showError('please check your number')
   }
 
-  e.preventDefault();
+  
+  //e.preventDefault();
 }
 
 
 function showError(error){
-  const errorDiv=document.createElement('div');
 
+  
+  document.getElementById('loading').style.display='none';
+  document.getElementById('result').style.display='none';
+
+  const errorDiv=document.createElement('div');
   //get elements
   const card=document.querySelector('.card');
   const heading =document.querySelector('.heading')
 
   errorDiv.className='alert alert-danger';
-
   errorDiv.appendChild(document.createTextNode(error)); 
-
   card.insertBefore(errorDiv,heading);
-
   setTimeout(clearError,3000);
 }
 

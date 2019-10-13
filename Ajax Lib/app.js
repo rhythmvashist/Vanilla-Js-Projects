3dd1c1651e2e1501e89ebd08458b9de188bbc556
Rhythmvashist <vashistrhythm1@gmail.com>
2019-10-12 18:37:58 -0700

    added post,put and delte request method in custome lib

--- Ajax Lib/app.js
+++ Ajax Lib/app.js
@@ -1,9 +1,27 @@
 lib=new  LibraryHttp();
-lib.get('https://jsonplaceholder.typicode.com/posts',function (err,serverResponse) {
+url='https://jsonplaceholder.typicode.com/posts';
+// lib.get('https://jsonplaceholder.typicode.com/posts',function (err,serverResponse) {
+//     if(err){
+//         console.log(err);
+//     }
+//     else {
+//         console.log(serverResponse);
+//     }
+// });
+
+//POST
+const data ={
+    title:'custom title',
+    body:' this is the body of '
+};
+
+lib.post(url,data,function (err,resp) {
     if(err){
         console.log(err);
-    }
-    else {
-        console.log(serverResponse);
+    }else{
+        console.log(resp);
     }
 });
+
+
+
--- Ajax Lib/lib.js
+++ Ajax Lib/lib.js
@@ -8,7 +8,44 @@ LibraryHttp.prototype.get=function (url,callback) {
     self=this;
     this.http.onload=function () {
         if(self.http.status===200){
-            callback(self.http.responseText);
+            callback(null,self.http.responseText);
+        }else{
+            callback("Error "+ self.status);
+        }
+    };
+    this.http.send();
+};
+
+
+// make a Post request
+LibraryHttp.prototype.post=function (url,data,callback) {
+    this.http.open('POST',url,true)
+    this.http.setRequestHeader('Content-type','application/json');
+    self=this;
+    this.http.onload=function () {
+        callback(null,self.http.responseText);
+    };
+    this.http.send(JSON.stringify(data));
+};
+
+
+LibraryHttp.prototype.put=function (url,data,callback) {
+    this.http.open('POST',url,true)
+    this.http.setRequestHeader('Content-type','application/json');
+    self=this;
+    this.http.onload=function () {
+        callback(null,self.http.responseText);
+    };
+    this.http.send(JSON.stringify(data));
+};
+
+
+LibraryHttp.prototype.delete=function (url,callback) {
+    this.http.open('GET',url,true);
+    self=this;
+    this.http.onload=function () {
+        if(self.http.status===200){
+            callback(null,'Post deleted ');
         }else{
             callback("Error "+ self.status);
         }
